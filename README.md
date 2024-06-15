055ddf4904033cce348941b93f8d2f6416048a92
vertexflows <vertexflows@gmail.com>
2024-06-14 23:03:42 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Lyn Dizon Professional Life Coaching
+
+### Professional Life Coaching website designed to aid clients in better improving their overall life styles
+  
+  ## Table of Contents:
+  ###  [Installation](#installation)
+  ###  [Usage](#usage)
+  ###  [Contributors](#contributors)
+  ###  [Tests](#tests)
+  ###  [Questions](#questions)
+  ## Installation:
+  ### Please install the following for the app to function:
+  ### n/a
+  ## Usage:
+  ### Life Coaching 
+  ## Contributors:
+  ### Lyn Dizon
+  
+  <img width="745" alt="lynCo" src="https://user-images.githubusercontent.com/84059980/235498443-d5f97c39-03ad-4049-9c9c-398fdc204f1b.png">
+  
+  ## Tests:
+  ### Please run the following commands to test:
+  ### no tests
+  ## Questions:
+  ### For additional questions, please contact me at 
+  ### Github: https://github.com/edlenh
+  ### or
+  ### Email: herreraed93@gmail.com
+
+
+<img width="858" alt="Screenshot 2023-05-01 103800" src="https://user-images.githubusercontent.com/84059980/235498558-a7f203fe-d2be-433f-9394-edd49fa04d00.png">
---
+++ main.js
@@ -0,0 +1,63 @@
+const primaryHeader = document.querySelector('.primary-header')
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', ()=>{
+    primaryNav.hasAttribute('data-visible')
+     ? navToggle.setAttribute('aria-expanded', false)
+     : navToggle.setAttribute('aria-expanded', true)
+    primaryNav.toggleAttribute('data-visible');
+    primaryHeader.toggleAttribute('data-overlay')
+})
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: false,
+    dots: true,
+    centerMode:true,
+    arrows:false,
+    responsive: {
+        480: {
+          dots: false // dots enabled 1280px and up
+        }
+      }
+  });
+
+
+var pause = 2;
+var speed = 2;
+var texts = ['Do you feel stuck and confused about life?' ,
+ 'Do you feel dissatisfied with where you are in life?',
+ 'Do you desire change in your life?',
+ 'if so...',
+"are you ready to create the life that you don't need to escape from?"
+];
+var subtext =  "start creating the life that you don't need to escape from.";
+
+(function() {
+  var text = texts[0];
+  var cur = 0,
+    dir = 1,
+    cur_text = 0;
+
+  var s = 0;
+
+  setInterval(function loop() {
+    cur += dir;
+    if (cur < 0) {
+      cur = 0;
+      dir = -dir;
+      cur_text = (++cur_text) % 5;
+      text = texts[cur_text];
+    }
+    if (cur > text.length) {
+      cur = text.length;
+      if (s++ > pause) {
+        s = 0
+        dir = -dir;
+      }
+    }
+    document.querySelector("#animation").setAttribute('class', text.toLowerCase())
+    document.querySelector("#animation").innerHTML = text.substring(0, cur);
+  }, 100 / speed)
+
+})()
